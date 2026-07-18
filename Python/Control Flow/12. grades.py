2a68d28c0090af76749a4e54b7b57f17f8ee426d
Ahmed <106522034@student.swin.edu.au>
2026-07-18 22:27:48 +1000

    18/07/26

---
+++ Python/Control Flow/12. grades.py
@@ -0,0 +1,8 @@
+# Write code below 💖
+
+grade = 80
+
+if grade >= 55:
+  print("You Passed.")
+else:
+  print("You Failed.")
---
+++ Python/Control Flow/13. pH levels.py
@@ -0,0 +1,10 @@
+# Write code below 💖
+
+pH = int(input("Value between 0 & 14: "))
+
+if pH > 7:
+  print("Basic")
+elif pH < 7:
+  print("Acidic")
+else:
+  print("Neutral")
