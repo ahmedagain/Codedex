b1131aff0901195a56a265f9d12e55d4b470cee9
Ahmed <106522034@student.swin.edu.au>
2026-07-27 17:14:24 +1000

    27/07/26

---
+++ Python/5. Lists/reading list.py
@@ -0,0 +1,9 @@
+# Write code below 💖
+
+reading_list = ['Harry Potter', '1984', 'The Fault in Our Stars', 'The Mom Test', 'Life in Code']
+
+reading_list.append("Pachinko")
+reading_list.remove('The Fault in Our Stars')
+reading_list.pop(1)
+
+print(reading_list)
---
+++ Python/5. Lists/to-do list.py
@@ -0,0 +1,7 @@
+# Write code below 💖
+
+to_do = ['🏦 Get quarters.', '🧺 Do laundry.', '🌳 Take a walk.', '💈 Get a haircut.', '🍵 Make some tea.', '💻 Complete Lists chapter.', '💖 Call mom.', '📺 Watch My Hero Academia.']
+print(to_do[0])
+print(to_do[1])
+print(to_do[3:5])
+print(to_do[9])
